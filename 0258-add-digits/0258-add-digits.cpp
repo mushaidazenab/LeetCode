3eee16afc0669e8737d6f9894328d1ce8e304d21
Mushaida Zenab <zmushaida@gmail.com>
2026-08-23 22:38:24 +0500

    Time: 3 ms (3.4%), Space: 8.5 MB (98.83%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+int addDigits(int num)
+{
+    // recursive soln
+    if (num <= 9)
+        return num;
+    int total = 0;
+    while (num != 0)
+    {
+        total += num % 10;
+        num /= 10;
+    }
+    return addDigits(total);
+}
+};
