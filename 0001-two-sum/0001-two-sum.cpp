aa522e29b750080a1eb612d0e79c89597ca6101e
Mushaida Zenab <zmushaida@gmail.com>
2026-09-10 23:16:25 +0500

    Time: 3 ms (67.32%), Space: 14.8 MB (44.58%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        for(int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            
+            auto it = seen.find(complement);
+            
+            if(it != seen.end()){
+                return {it->second, i}; 
+            }
+            
+            seen[nums[i]] = i;  
+        }
+        return {};
+    }
+};
