5206da6c61346f3bd1b1b335e0c924c78f89d1f4
Mushaida Zenab <zmushaida@gmail.com>
2026-09-25 22:49:39 +0500

    Time: 0 ms (100%), Space: 8.6 MB (44.15%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+double solve(double x, long long n)
+{
+    if (n == 0)
+    {
+        return 1.0;
+    }
+    double half = solve(x, n / 2);
+    if (n % 2 == 0)
+    {
+        return half * half;
+    }
+    else
+    {
+        return x * half * half;
+    }
+}
+double myPow(double x, int n)
+{
+    long long N = n;
+    if (N < 0)
+    {
+        return 1.0 / solve(x, -N);
+    }
+    return solve(x, N);
+}
+};
